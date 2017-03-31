1ea15c940f17beb883edb4e9555d54ef284314d3
Daniele Perazzolo <dperazzo@sfu.ca>
2017-03-31 00:36:13 -0700

    Install isotopejs.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 hljs.initHighlightingOnLoad();
+
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
